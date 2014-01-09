386fed039032c918cd5142dec803b1a5635455a8
Asvarduil <andrew.russel.gray@gmail.com>
2014-01-08 18:28:12 -0600

    Implemented 'You Win' screen and script to return to title screen.  Moved GUI scripts to GUI folder.  Removed subfolders from GUI asset folder.

---
+++ Assets/Game-Specific Assets/Scripts/GUI/GameOverGUI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverGUI : MonoBehaviour 
+{
+	#region Variables / Properties
+
+	public bool DebugMode = false;
+	public GUISkin Skin;
+	public AsvarduilImage YouWinPane;
+	public AsvarduilButton TitleButton;
+
+	private Fader _fader;
+
+	#endregion Variables / Properties
+
+	#region Engine Hooks
+
+	public void Start()
+	{
+		_fader = (Fader) GameObject.FindObjectOfType(typeof(Fader));
+	}
+
+	public void OnGUI()
+	{
+		GUI.skin = Skin;
+
+		YouWinPane.DrawMe();
+
+		if(TitleButton.IsClicked())
+		{
+			StartCoroutine(ReturnToTitle());
+		}
+	}
+
+	public void Update()
+	{
+		YouWinPane.Tween();
+		TitleButton.Tween();
+	}
+
+	#endregion Engine Hooks
+
+	#region Methods
+
+	private IEnumerator ReturnToTitle()
+	{
+		_fader.FadeOut();
+		while(_fader.ScreenShown)
+			yield return 0;
+
+		Application.LoadLevel("Title");
+	}
+
+	#endregion Methods
+}
