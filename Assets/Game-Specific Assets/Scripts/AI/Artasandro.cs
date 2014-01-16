72a2c910ecbf24abf3a873ae6116454dd61f4799
Asvarduil <andrew.russel.gray@gmail.com>
2014-01-15 19:15:15 -0600

    Added Quadramagic Shot for Artasandro and possibly Barbariccia boss fights.  Wrote Projectile Group script for such multi-shots.

---
+++ Assets/Game-Specific Assets/Scripts/AI/Artasandro.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Artasandro : AIBase
+{
+	#region Variables / Properties
+
+
+
+	#endregion Variables / Properties
+
+	#region Engine Hooks
+
+	public void FixedUpdate()
+	{
+		if(_isPaused)
+			return;
+
+
+		PlayAnimations();
+	}
+
+	#endregion Engine Hooks
+
+	#region Methods
+
+	#endregion Methods
+}
---
+++ Assets/Game-Specific Assets/Scripts/System/ProjectileGroup.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ProjectileGroup : MonoBehaviour 
+{
+	#region Variables / Properties
+
+	public bool DebugMode = false;
+	public int ProjectileCount;
+
+	#endregion Variables / Properties
+
+	#region Engine Hooks
+
+	public void Start()
+	{
+		CheckProjectileCount();
+	}
+
+	public void Update()
+	{
+		CheckProjectileCount();
+		if(ProjectileCount == 0)
+		{
+			if(DebugMode)
+				Debug.Log(gameObject.name + " has no more children; self-destructing.");
+
+			Destroy(gameObject);
+		}
+	}
+
+	#endregion Engine Hooks
+
+	#region Methods
+
+	private void CheckProjectileCount()
+	{
+		ProjectileCount = 0;
+		foreach(Transform child in transform) 
+		{
+			ProjectileCount++;
+		}
+	}
+
+	#endregion Methods
+}
