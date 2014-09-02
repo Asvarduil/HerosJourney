b6ce0201e06a89ca6a9ae54b09f26dba45feb78f
Asvarduil <andrew.russel.gray@gmail.com>
2014-09-01 21:12:51 -0500

    Added visual effect for the location of the next main quest objective.  Reimported in latest version of Unity.

---
+++ Assets/Game-Specific Assets/Scripts/System/ObjectiveSparkle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectiveSparkle : MonoBehaviour 
+{
+	#region Variables / Properties
+
+	public List<SequenceCounter> AvailableSequences;
+
+	private Ambassador _ambassador;
+
+	#endregion Variables / Properties
+
+	#region Events
+
+	public void Start()
+	{
+		_ambassador = Ambassador.Instance;
+
+		EnableEffect();
+	}
+
+	#endregion Events
+
+	#region Methods
+
+	public void EnableEffect()
+	{
+		bool showParticles = false;
+		foreach(SequenceCounter sequence in AvailableSequences)
+		{
+			showParticles = showParticles || _ambassador.CheckThreadProgress(sequence.Name, sequence.Phase);
+		}
+		
+		particleEmitter.emit = showParticles;
+	}
+
+	#endregion Methods
+}
